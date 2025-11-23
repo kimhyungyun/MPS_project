eff40eacdce236badb5948146460bdb5c181beee
kimhyungyun <gurbsdk@naver.com>
2025-11-23 13:05:29 +0900

    1305

--- app/mpspain/mpschamp/page.tsx
+++ app/mpspain/mpschamp/page.tsx
@@ -91,7 +91,7 @@ const MpsChamp = () => {
               d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
             />
           </svg>
-          <span className="text-2xl font-bold text-gray-800">공지사항</span>
+          <span className="text-2xl font-bold text-gray-800">MPS 캠프 안내입니다</span>
         </div>
         {user.mb_level >= 8 && (
           <button
