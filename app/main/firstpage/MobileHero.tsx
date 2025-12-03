933b9d77bc2eaefcfa726d7a2b9140b7ac62c962
kimhyungyun <gurbsdk@naver.com>
2025-12-03 22:01:13 +0900

    2201

--- app/main/firstpage/MobileHero.tsx
+++ app/main/firstpage/MobileHero.tsx
@@ -7,7 +7,7 @@ import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
-const images = ["/메인사진1.png", "/메인사진2.jpg"];
+const images = ["/테스트이미지2.png", "/메인사진2.jpg"];
 
 export default function MobileHero() {
   return (
