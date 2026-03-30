3d6e25b993d0803df7bb7ea3e14ae9a56fed935a
kimhyungyun <gurbsdk@naver.com>
2026-03-30 13:21:42 +0900

    1321

---
+++ app/mpspain/mpschamp/campPromotion/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-white">
+      <h1 className="text-5xl font-bold text-black">
+        큐알 홍보데스크
+      </h1>
+    </main>
+  );
+}
